20e0c15c5d606e4d85aaf1744c96c7d6dc395c82
EZhitnikovich <ezhitnikovich@gmail.com>
2021-01-03 20:57:43 +0300

    Add project files.

---
+++ 30-Days-of-Code/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Day_0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputString = Console.ReadLine();
+
+            Console.WriteLine($"Hello, World.\n" +
+                              $"{inputString}");
+        }
+    }
+}
