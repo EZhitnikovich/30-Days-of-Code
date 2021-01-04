1bb204319097de47451ef78eb04ead1abaa5c189
EZhitnikovich <ezhitnikovich@gmail.com>
2021-01-04 19:25:33 +0300

    add day 1 task

---
+++ Day_1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Day_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 4;
+            double d = 4.0;
+            string s = "HackerRank ";
+
+            int inputI = int.Parse(Console.ReadLine());
+            double inputD = double.Parse(Console.ReadLine());
+            string inputS = Console.ReadLine();
+
+            Console.WriteLine($"{i+inputI}\n" +
+                              $"{d+inputD}\n" +
+                              $"{s+inputS}");
+        }
+    }
+}
