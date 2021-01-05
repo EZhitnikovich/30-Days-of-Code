dfa29b44a2cf28ba34018910d0b47a60badd9ea1
EZhitnikovich <ezhitnikovich@gmail.com>
2021-01-05 16:48:32 +0300

    add day 2 task

---
+++ Day_2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Day_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double mealCost = Convert.ToDouble(Console.ReadLine());
+            int tipPercent = Convert.ToInt32(Console.ReadLine());
+            int taxPercent = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(Math.Round(GetTotalCost(mealCost, tipPercent, taxPercent)));
+        }
+
+        static double GetTotalCost(double cost, int tipPercent, int taxPercet)
+        {
+            double tip = tipPercent / 100d * cost;
+            double tax = taxPercet / 100d * cost;
+
+            return cost + tip + tax;
+        }
+    }
+}
