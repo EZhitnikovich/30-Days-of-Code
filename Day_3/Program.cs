5a269faa8ce6bce70cb3861344a5e41502b70aad
EZhitnikovich <ezhitnikovich@gmail.com>
2021-01-06 15:27:15 +0300

    add day 3 task

---
+++ Day_3/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Day_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(WeirdOrNotWeird(n));
+        }
+
+        static string WeirdOrNotWeird(int n)
+        {
+            string result = string.Empty;
+
+            if (n % 2 == 1)
+            {
+                result = "Weird";
+            }
+            else
+            {
+                if (n >= 2 && n <= 5)
+                {
+                    result = "Not Weird";
+                }
+                else if (n>=6 && n <= 20)
+                {
+                    result = "Weird";
+                }
+                else if (n > 20)
+                {
+                    result = "Not Weird";
+                }
+            }
+
+            return result;
+        }
+    }
+}
