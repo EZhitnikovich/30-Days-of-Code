6e69a2391d22e17a779671ab1160c374381d8ee7
EZhitnikovich <ezhitnikovich@gmail.com>
2021-01-09 16:18:51 +0300

    add day 6 task

---
+++ Day_6/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+
+namespace Day_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var count = int.Parse(Console.ReadLine());
+
+            for(int i = 0; i < count; i++)
+            {
+                string word = Console.ReadLine();
+                string even = GetEvenLetters(word);
+                string odd = GetOddLetters(word);
+                Console.WriteLine($"{even} {odd}");
+            }
+        }
+
+        static string GetEvenLetters(string word)
+        {
+            string result = string.Empty;
+
+            for(int i = 0; i < word.Length; i += 2)
+            {
+                result += Convert.ToString(word[i]);
+            }
+
+            return result;
+        }
+
+        static string GetOddLetters(string word)
+        {
+            string result = string.Empty;
+
+            for (int i = 1; i < word.Length; i += 2)
+            {
+                result += Convert.ToString(word[i]);
+            }
+
+            return result;
+        }
+    }
+}
