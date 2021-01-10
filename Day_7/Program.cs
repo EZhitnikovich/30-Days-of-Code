9aa435286ca99a1b324e34407aa8e6876b86c08c
EZhitnikovich <ezhitnikovich@gmail.com>
2021-01-10 17:14:48 +0300

    add day 7 task

---
+++ Day_7/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Day_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int len = int.Parse(Console.ReadLine());
+            int[] arr = Array.ConvertAll(Console.ReadLine().Split(' '), arrTemp => Convert.ToInt32(arrTemp))[0..len];
+            Console.WriteLine(string.Join(" ", ReverseArray(arr)));
+        }
+
+        static IEnumerable<T> ReverseArray<T>(IEnumerable<T> arr)
+            => arr.Reverse();
+    }
+}
