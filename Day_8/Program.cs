61a6d6291a582ed58e05eda90b147a25438f234c
EZhitnikovich <ezhitnikovich@gmail.com>
2021-01-11 22:06:51 +0300

    add day 8 task

---
+++ Day_8/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Day_8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> phoneBook = new Dictionary<string, string>();
+
+            int count = int.Parse(Console.ReadLine());
+
+            for(int i = 0; i < count; i++)
+            {
+                var temp = Console.ReadLine().Split(' ');
+                phoneBook.Add(temp[0], temp[1]);
+            }
+
+            for(int i = 0; i < count; i++)
+            {
+                var name = Console.ReadLine();
+                FindNumberByName(phoneBook, name);
+            }
+        }
+
+        static void FindNumberByName(Dictionary<string, string> phoneBook, string name)
+        {
+            string result = string.Empty;
+            if(phoneBook.TryGetValue(name, out string number))
+            {
+                result = $"{name}={number}";
+            }
+            else
+            {
+                result = "Not found";
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}
