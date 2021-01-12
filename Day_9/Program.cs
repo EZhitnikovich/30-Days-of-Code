0f4712224c51e80bc4fcde8d2f7e89eb5174eb6f
EZhitnikovich <ezhitnikovich@gmail.com>
2021-01-12 18:58:40 +0300

    add day 9 task

---
+++ Day_9/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Day_9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine(Factorial(n));
+        }
+
+        static int Factorial(int n)
+        {
+            if (n == 0)
+            {
+                return 1;
+            }
+            else
+            {
+                return n * Factorial(n - 1);
+            }
+        }
+    }
+}
