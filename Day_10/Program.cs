1d779b704e5032eea4d7e21558f29cc2cf1da388
EZhitnikovich <ezhitnikovich@gmail.com>
2021-01-13 16:06:45 +0300

    add day 10 task

---
+++ Day_10/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+namespace Day_10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine(CountBytes(n));
+        }
+
+        static int CountBytes(int n)
+            => Convert.ToString(n, 2).Split('0').Max(x=>x.Length);
+    }
+}
