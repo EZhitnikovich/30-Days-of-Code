ac116357370feab30d389ab3feef8bc2e1fe358c
EZhitnikovich <ezhitnikovich@gmail.com>
2021-01-15 14:19:10 +0300

    add day 12 task

---
+++ Day_12/Program.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Linq;
+
+class Person
+{
+	protected string firstName;
+	protected string lastName;
+	protected int id;
+
+	public Person() { }
+	public Person(string firstName, string lastName, int identification)
+	{
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.id = identification;
+	}
+	public void printPerson()
+	{
+		Console.WriteLine("Name: " + lastName + ", " + firstName + "\nID: " + id);
+	}
+}
+
+class Student : Person
+{
+
+	private int[] testScores;
+
+	/// <summary>
+	/// Parameters: 
+    ///   firstName - A string denoting the Person's first name.
+    ///   lastName - A string denoting the Person's last name.
+    ///   id - An integer denoting the Person's ID number.
+    ///   scores - An array of integers denoting the Person's test scores.
+	/// </summary>
+	/// <param name="firstName"></param>
+	/// <param name="lastName"></param>
+	/// <param name="id"></param>
+	/// <param name="scores"></param>
+	public Student(string firstName, string lastName, int id, int[] scores): base(firstName, lastName, id)
+    {
+		testScores = scores;
+	}
+
+	/// <summary>
+	/// </summary>
+	/// <returns>
+	/// A character denoting the grade.
+	/// </returns>
+	public char Calculate()
+    {
+		int avarage = testScores.Sum() / testScores.Count();
+
+		char result = default(char);
+
+		if(avarage >= 90 && avarage <= 100)
+        {
+			result = 'O';
+        }
+		else if(avarage >= 80 && avarage < 90)
+        {
+			result = 'E';
+        }
+		else if(avarage >= 70 && avarage < 80)
+        {
+			result = 'A';
+        }
+		else if(avarage >= 55 && avarage < 70)
+        {
+			result = 'P';
+        }
+		else if(avarage >= 40 && avarage < 55)
+        {
+			result = 'D';
+        }
+		else if(avarage >= 0 && avarage < 40)
+        {
+			result = 'T';
+        }
+
+		return result;
+    }
+}
+
+class Solution
+{
+	static void Main()
+	{
+		string[] inputs = Console.ReadLine().Split();
+		string firstName = inputs[0];
+		string lastName = inputs[1];
+		int id = Convert.ToInt32(inputs[2]);
+		int numScores = Convert.ToInt32(Console.ReadLine());
+		inputs = Console.ReadLine().Split();
+		int[] scores = new int[numScores];
+		for (int i = 0; i < numScores; i++)
+		{
+			scores[i] = Convert.ToInt32(inputs[i]);
+		}
+
+		Student s = new Student(firstName, lastName, id, scores);
+		s.printPerson();
+		Console.WriteLine("Grade: " + s.Calculate() + "\n");
+	}
+}
---
+++ Day_12/Readme.md
@@ -0,0 +1,11 @@
+﻿# Task
+You are given two classes, Person and Student, where Person is the base class and Student is the derived class. Completed code for Person and a declaration for Student are provided for you in the editor. Observe that Student inherits all the properties of Person.
+
+Complete the Student class by writing the following:
+
+* A Student class constructor, which has *4* parameters:
+	1. A string, *firstName*.
+	1. A string, *lastName*.
+	1. An integer, *idNumber*.
+	1. An integer array (or vector) of test scores, *scores*.
+* A char calculate() method that calculates a Student object's average and returns the grade character representative of their calculated average
