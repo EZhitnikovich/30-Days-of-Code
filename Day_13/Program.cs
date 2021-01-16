be8f8871489b3fee727ae5a6898c0143886aaf41
EZhitnikovich <ezhitnikovich@gmail.com>
2021-01-16 19:45:47 +0300

    add day 13 task

---
+++ Day_13/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+abstract class Book
+{
+
+    protected string title;
+    protected string author;
+
+    public Book(string t, string a)
+    {
+        title = t;
+        author = a;
+    }
+    public abstract void display();
+
+
+}
+
+class MyBook: Book
+{
+    public int Price { get; set; }
+    public MyBook(string title, string author, int price): base(title, author)
+    {
+        Price = price;
+    }
+
+    public override void display()
+    {
+        Console.WriteLine($"Title: {title}\n" +
+                          $"Author: {author}\n" +
+                          $"Price: {Price}");
+    }
+}
+
+class Solution
+{
+    static void Main(string[] args)
+    {
+        string title = Console.ReadLine();
+        string author = Console.ReadLine();
+        int price = Int32.Parse(Console.ReadLine());
+        Book new_novel = new MyBook(title, author, price);
+        new_novel.display();
+    }
+}
---
+++ Day_13/Readme.md
@@ -0,0 +1,12 @@
+﻿# Task
+Given a Book class and a Solution class, write a MyBook class that does the following:
+
+* Inherits from Book
+* Has a parameterized constructor taking these  parameters:
+	1. string *title*
+	1. string *author*
+	1. int *price*
+* Implements the Book class' abstract display() method so it prints these *3* lines:
+	1. *Title:*, a space, and then the current instance's *title*.
+	1. *Author:*, a space, and then the current instance's *author*.
+	1. *Price:*, a space, and then the current instance's *price*.
