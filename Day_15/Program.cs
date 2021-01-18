94fc57138da37dbee24a1307472aa3d5db757805
EZhitnikovich <ezhitnikovich@gmail.com>
2021-01-18 22:57:13 +0300

    complete day 15 task

--- Day_15/Program.cs
+++ Day_15/Program.cs
@@ -15,7 +15,20 @@ class Solution
 
 	public static Node insert(Node head, int data)
 	{
-		//Complete this method
+		if(head == null)
+        {
+			return new Node(data);
+        }
+        else
+        {
+			Node temp = head;
+            while (temp.next != null)
+            {
+				temp = temp.next;
+            }
+			temp.next = new Node(data);
+			return head;
+		}
 	}
 
 	public static void display(Node head)
