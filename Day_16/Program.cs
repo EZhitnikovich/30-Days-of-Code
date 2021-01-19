d3f2b95a64246af8f770efe543d97db8b405ec64
EZhitnikovich <ezhitnikovich@gmail.com>
2021-01-19 21:38:30 +0300

    add day 16 task

---
+++ Day_16/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Day_16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string S = Console.ReadLine();
+
+            try
+            {
+                Console.WriteLine(int.Parse(S));
+            }
+            catch
+            {
+                Console.WriteLine("Bad String");
+            }
+        }
+    }
+}
---
+++ Day_16/Readme.md
@@ -0,0 +1,3 @@
+﻿# Task
+
+Read a string, *S*, and print its integer value; if *S* cannot be converted to an integer, print Bad String.
