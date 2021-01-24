853eeefc677b5e2e3343967631ab243482c59ae6
EZhitnikovich <ezhitnikovich@gmail.com>
2021-01-24 20:40:09 +0300

    add day 21 task

---
+++ Day_21/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Printer
+{
+
+	public static void PrintArray<T>(IEnumerable<T> arr)
+	{
+		Console.WriteLine(string.Join("\n", arr));
+	}
+
+
+	static void Main(string[] args)
+	{
+		int n = Convert.ToInt32(Console.ReadLine());
+		int[] intArray = new int[n];
+		for (int i = 0; i < n; i++)
+		{
+			intArray[i] = Convert.ToInt32(Console.ReadLine());
+		}
+
+		n = Convert.ToInt32(Console.ReadLine());
+		string[] stringArray = new string[n];
+		for (int i = 0; i < n; i++)
+		{
+			stringArray[i] = Console.ReadLine();
+		}
+
+		PrintArray<int>(intArray);
+		PrintArray<string>(stringArray);
+	}
+}
---
+++ Day_21/Readme.md
@@ -0,0 +1,2 @@
+﻿# Task
+Write a single generic function named printArray; this function must take an array of generic elements as a parameter (the exception to this is C++, which takes a vector).
