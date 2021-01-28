9b1034fa401c65ff3ac1989b902127f731bc54b7
EZhitnikovich <ezhitnikovich@gmail.com>
2021-01-28 13:43:37 +0300

    add day 25 task

---
+++ Day_25/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Day_25
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int len = int.Parse(Console.ReadLine());
+
+            List<int> nums = new List<int>();
+
+            for(int i = 0; i< len; i++)
+            {
+                nums.Add(int.Parse(Console.ReadLine()));
+            }
+
+            foreach(var elem in nums)
+            {
+                string result = IsPrime(elem) ? "Prime" : "Not prime";
+                Console.WriteLine(result);
+            }
+        }
+
+        static bool IsPrime(int num)
+        {
+            int countDivisors = Enumerable.Range(1, num).Where(x => num % x == 0).Count();
+            return countDivisors == 2 ? true : false;
+        }
+    }
+}
---
+++ Day_25/Readme.md
@@ -0,0 +1,1 @@
+﻿A prime is a natural number greater than *1* that has no positive divisors other than *1* and itself. Given a number, *n*, determine and print whether it is *Prime* or *Not prime*.
